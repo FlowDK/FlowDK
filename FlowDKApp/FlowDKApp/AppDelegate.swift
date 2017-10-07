181799d56aba8842864a3cbdce09dd19700ee0a6
JP McGlone <jp@trifl.co>
2017-10-07 16:18:03 -0400

    Add an example app back to the repo

---
+++ FlowDKApp/FlowDKApp/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import FlowDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow.window(rootViewController: ViewController(), makeKeyAndVisible: true)
+    
+    return true
+  }
+}
