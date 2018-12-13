7bf6c54a10c9d6e76bb5312c719d3d1e1d346322
JP McGlone <john.mcglone@verum.me>
2018-12-13 18:00:34 -0500

    update to addingPercentEncoding

--- FlowDK/Core/Classes/Extensions/Foundation/String+Additions.swift
+++ FlowDK/Core/Classes/Extensions/Foundation/String+Additions.swift
@@ -2,11 +2,6 @@ public extension String {
   public var url: URL? { return URL(string: self) }
   
   public var urlEncoded: String {
-    return CFURLCreateStringByAddingPercentEscapes(
-      nil,
-      self as CFString?,
-      nil,
-      "!*'();:@&=+$,/?%#[]" as CFString?,CFStringBuiltInEncodings.UTF8.rawValue
-      ) as String
+    return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? self
   }
 }
